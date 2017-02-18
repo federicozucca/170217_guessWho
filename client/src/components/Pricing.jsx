035af29bdfa32ea2d257569c95de3baedc8003c3
Federico Zucca <zucca.federico@gmail.com>
2017-02-18 10:08:28 +0000

    first commit

---
+++ client/src/components/Pricing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Pricing extends React.Component{
+  
+  render(){
+    return(
+      <div>
+        <h4> Pricing </h4>
+        <p>  ££££££ </p>
+      </div>
+    )
+  }
+  
+}
+
+module.exports = Pricing;
+
